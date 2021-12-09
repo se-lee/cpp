32caab23306f2ff3cae8e5e9abf4ef3affb02736
selee <selee@student.42lyon.fr>
2021-12-09 01:19:09 +0100

    removed char *str variable

--- CPP_module00/ex00/megaphone.cpp
+++ CPP_module00/ex00/megaphone.cpp
@@ -1,22 +1,9 @@
-/*
-小文字を大文字に変えるやつ
-
-$>./megaphone "shhhhh... I think the students are asleep..."
-SHHHHH... I THINK THE STUDENTS ARE ASLEEP...
-$>./megaphone Damnit " ! " "Sorry students, I thought this thing was off."
-DAMNIT ! SORRY STUDENTS, I THOUGHT THIS THING WAS OFF.
-$>./megaphone
-* LOUD AND UNBEARABLE FEEDBACK NOISE *
-
-*/
-
 #include <iostream>
 
 int		main(int argc, char **argv)
 {
 	int		i;
 	int		j;
-	char	*str;
 
 	i = 1;
 	if (argc == 1)
@@ -26,10 +13,9 @@ int		main(int argc, char **argv)
 		while (argv[i])
 		{
 			j = 0;
-			str = argv[i];
-			while (str[j])
+			while (argv[i][j])
 			{
-				std::cout << (char)toupper(str[j]);
+				std::cout << (char)toupper(argv[i][j]);
 				j++;
 			}
 			i++;

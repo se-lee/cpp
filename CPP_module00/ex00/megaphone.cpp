51c94537ac83191a9176bbe3dafae6aa3c12f2d4
selee <selee@student.42lyon.fr>
2021-12-09 01:15:26 +0100

    main done, need to work on Makefile

--- CPP_module00/ex00/megaphone.cpp
+++ CPP_module00/ex00/megaphone.cpp
@@ -10,3 +10,32 @@ $>./megaphone
 
 */
 
+#include <iostream>
+
+int		main(int argc, char **argv)
+{
+	int		i;
+	int		j;
+	char	*str;
+
+	i = 1;
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		while (argv[i])
+		{
+			j = 0;
+			str = argv[i];
+			while (str[j])
+			{
+				std::cout << (char)toupper(str[j]);
+				j++;
+			}
+			i++;
+		}
+		std::cout << std::endl;
+	}
+	return (0);
+}
+

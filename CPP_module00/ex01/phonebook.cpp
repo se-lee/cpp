d398ca0d2406c505c186ecdec12ec8c9020207d5
selee <selee@student.42lyon.fr>
2021-12-10 01:51:36 +0100

    i don't know where i'm going...

---
+++ CPP_module00/ex01/phonebook.cpp
@@ -0,0 +1,40 @@
+/*
+When the program starts, the user is prompted for input: 
+you should accept the ADD command, the SEARCH command or the EXIT command. 
+Any other input is discarded
+
+*/
+
+#include "phonebook.hpp"
+
+
+//add_contact_info
+void	add_contact_info()
+{
+	
+}
+
+//search_contact_info
+
+//exit_phonebook
+
+int	main()
+{
+
+// prompt: get input
+/*
+	while (what)
+	if command is EXIT
+		exit_program, contacts are lost forever
+	else if command is ADD
+		add_contact_info
+	else if command is SEARCH
+		search_contact
+	else
+		input is discarded
+		(display some message)
+*/
+
+}
+
+
---
+++ CPP_module00/ex01/phonebook.hpp
@@ -0,0 +1,24 @@
+#ifndef PHONEBOOK_HPP
+# define PHONEBOOK_HPP
+
+# include <iostream>
+# include <iomanip>
+# include <string>
+
+
+/*
+
+first_name;
+last_name;
+nickname;
+phone_number;
+darkest_secret;
+
+*/
+class contact_info {
+	std::string	first_name;
+
+}
+
+
+#endif

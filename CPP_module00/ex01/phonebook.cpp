204c8f962a37f87b47edf5332c7cdfd7a2bd88bd
Seoyoung Lee <selee@student.42lyon.fr>
2021-12-11 18:25:58 +0100

    -

--- CPP_module00/ex01/phonebook.cpp
+++ CPP_module00/ex01/phonebook.cpp
@@ -3,12 +3,25 @@ When the program starts, the user is prompted for input:
 you should accept the ADD command, the SEARCH command or the EXIT command. 
 Any other input is discarded
 
+
+[ADD]
+◦ The program will prompt the user to input a new contact’s information, one
+field at a time, until every field is accounted for.
+◦ A contact is defined by the following fields: first name, last name, nickname,
+phone number,darkest secret.
+◦ The PhoneBook must be represented as as an instance of a class in your code
+it must contain an array of contact.
+◦ A contact MUST be represented as an instance of a class in your code. You’re
+free to design the class as you like, but the peer evaluation will check the
+consistency of your choices. Go look at today’s videos again if you don’t
+understand what I mean (and I don’t mean "use everything" before you ask).
+
+
+
 */
 
 #include "phonebook.hpp"
 
-
-//add_contact_info
 void	add_contact_info()
 {
 	
--- CPP_module00/ex01/phonebook.hpp
+++ CPP_module00/ex01/phonebook.hpp
@@ -5,18 +5,16 @@
 # include <iomanip>
 # include <string>
 
+class contact {
+	std::string	first_name;
+	std::string	last_name;
+	std::string	nickname;
+	std::string	phone_number;
+	std::string	darkest_secret;
 
 /*
 
-first_name;
-last_name;
-nickname;
-phone_number;
-darkest_secret;
-
 */
-class contact_info {
-	std::string	first_name;
 
 }
 

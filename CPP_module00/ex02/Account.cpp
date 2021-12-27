c73a66c2d1bc684d29fcd2740f1e7f4e5dac7037
Seoyoung Lee <selee@student.42lyon.fr>
2021-12-27 20:03:06 +0100

    ex02 mada nanimo shitenai

---
+++ CPP_module00/ex02/Account.cpp
@@ -0,0 +1,52 @@
+#include "Account.hpp"
+
+
+static int	Account::getNbAccounts(void)
+{
+
+}
+
+static int	Account::getTotalAmount(void)
+{
+
+}
+
+static int	Account::getNbDeposits(void)
+{
+
+}
+
+static int	Account::getNbWithdrawals(void)
+{
+
+}
+
+static void	Account::displayAccountsInfos(void)
+
+
+
+void	Account::makeDeposit(int deposit)
+{
+
+}
+
+bool	Account::makeWithdrawal(int withdrawal)
+{
+
+}
+
+int		Account::checkAmount( void ) const
+{
+
+}
+
+void	Account::displayStatus( void ) const
+{
+
+}
+
+static void	Account::_displayTimestamp( void )
+{
+
+}
+

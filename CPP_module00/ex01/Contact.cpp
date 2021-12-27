704951ee7fb24d12cf140e0498a4e4dcf10a5c82
Seoyoung Lee <selee@student.42lyon.fr>
2021-12-27 17:59:43 +0100

    a little [ ]

--- CPP_module00/ex01/Contact.cpp
+++ CPP_module00/ex01/Contact.cpp
@@ -18,16 +18,16 @@ Contact::~Contact(void)
 
 void	Contact::get_contact_info(void)
 {
-	std::cout << "Please enter the following: " << std::endl;
-	std::cout << "First name: ";
+	std::cout << "[Please enter the following]: " << std::endl;
+	std::cout << "[First name]: ";
 	std::cin >> this->_first_name;
-	std::cout << "Last name: ";
+	std::cout << "[Last name]: ";
 	std::cin >> this->_last_name;
-	std::cout << "Nickname: ";
+	std::cout << "[Nickname]: ";
 	std::cin >> this->_nickname;
-	std::cout << "Phone number: ";
+	std::cout << "[Phone number]: ";
 	std::cin >> this->_phone_number;
-	std::cout << "Darkest secret: ";
+	std::cout << "[Darkest secret]: ";
 	std::cin >> this->_darkest_secret;
 
 	return ;

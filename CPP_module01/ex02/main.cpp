ef8cd904f2fa18f2c6abc13bb9c786fa6c8864aa
Seoyoung Lee <selee@student.42lyon.fr>
2021-12-29 13:36:31 +0100

    cpp 01 ex 02 done

---
+++ CPP_module01/ex02/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main()
+{
+	std::string		str = "HI THIS IS BRAIN";
+	std::string		*strPTR = &str;
+	std::string		&strREF	= str;
+
+	std::cout << "---------> ADDRESS <---------" << std::endl;
+	std::cout << "[str   ] " << &str << std::endl;
+	std::cout << "[strPTR] " << strPTR << std::endl;
+	std::cout << "[strREF] " << &strREF << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "---------> STRING <---------" << std::endl;
+	std::cout << "[str   ] " << str << std::endl;
+	std::cout << "[strPTR] " << *strPTR << std::endl;
+	std::cout << "[strREF] " << strREF << std::endl;
+
+	return (0);
+}

c8d5ac559e4addcd098aea1169de0a21f5e2f56c
Seoyoung Lee <selee@student.42lyon.fr>
2022-01-14 19:56:21 +0100

    bureaucrat basics filled

---
+++ CPP_module05/ex00/Bureaucrat.hpp
@@ -0,0 +1,29 @@
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <iostream>
+# include <stdexcept>
+
+class  Bureaucrat
+{
+	private:
+		std::string const	_name;
+		int					_grade;
+
+	protected:
+
+
+	public:
+		Bureaucrat();
+		Bureaucrat(std::string const name, int grade);
+		~Bureaucrat();
+
+		std::string		getName(void);
+		std::ostream 	&operator<<(std::ostream &out);
+		int				getGrade(void);
+		void			incrementGrade(void);
+		void			decrementGrade(void);
+};
+
+
+#endif
---
+++ CPP_module05/ex00/main.cpp
@@ -0,0 +1,8 @@
+#include "Bureaucrat.hpp"
+
+int main()
+{
+	Bureaucrat bureau("test_name", 1);
+
+	return 0;
+}

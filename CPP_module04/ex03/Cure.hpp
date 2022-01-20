5ad32b28fc57746cc51876ebcd7467d33204a9e1
selee <selee@student.42lyon.fr>
2022-01-20 20:39:16 +0100

    -

---
+++ CPP_module04/ex03/Cure.hpp
@@ -0,0 +1,23 @@
+#ifndef CURE_HPP
+# define CURE_HPP
+
+# include <iostream>
+# include "AMateria.hpp"
+
+class Cure: public AMateria
+{
+	private:
+		
+
+	public:
+		Cure();
+		Cure()
+		~Cure();
+
+
+};
+
+
+
+
+#endif

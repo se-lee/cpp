51a176bbcea51a009425047c46ae5bc213b3d601
selee <selee@student.42lyon.fr>
2022-01-29 02:58:34 +0100

    ex01 done

--- CPP_module07/ex01/main.cpp
+++ CPP_module07/ex01/main.cpp
@@ -1,12 +1,33 @@
 #include "iter.hpp"
+#include <string>
+
+template < typename T >
+void	addTwo(T &arg)
+{
+	arg += 2;
+}
 
 int main( void )
 {
-	int array[5] = {1, 3, 4, 5, 6};
+/****************[ Int ]***********************/
+	int array[5] = {1, 2, 3, 4, 5};
 
 	for (int i = 0; i < 5; i++)
 		std::cout << array[i] << ", ";
 	std::cout << std::endl;
+
+	iter(array, 5, &addTwo);
+	for (int i = 0; i < 5; i++)
+		std::cout << array[i] << ", ";
+	std::cout << std::endl;
+
+/******************[ Char ]*********************/
+
+	char	str1[6] = "12345";
+
+	std::cout << str1 << std::endl;
+	iter(str1, strlen(str1), &addTwo);
+	std::cout << str1 << std::endl;
 
 	return 0;
 }

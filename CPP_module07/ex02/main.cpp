26e9164e96bdf7b4de473e4ad865be8b5b3adf3b
Seoyoung Lee <selee@student.42lyon.fr>
2022-03-14 13:11:14 +0100

    cpp7 ex2 fix

--- CPP_module07/ex02/main.cpp
+++ CPP_module07/ex02/main.cpp
@@ -1,6 +1,6 @@
 #include "Array.hpp"
 
-#define MAX_VAL 750
+#define MAX_VAL 10
 
 int main(int, char**)
 {
@@ -12,15 +12,23 @@ int main(int, char**)
 		const int value = rand();
 		numbers[i] = value;
 		mirror[i] = value;
+		std::cout << "value: " << value << " numbers: " << numbers[i] << " mirror: " << mirror[i] << std::endl;
 	}
 	//SCOPE
 	{
 		Array<int> tmp = numbers;
 		Array<int> test(tmp);
-	}
-
+	std::cout << "-------------------------------------------------------" << std::endl;
 	for (int i = 0; i < MAX_VAL; i++)
 	{
+		std::cout << "numbers: " << numbers[i] << " mirror: " << mirror[i] << std::endl;
+		std::cout << "tmp: " << tmp[i] << " test: " << test[i] << std::endl << std::endl;
+	}
+	}
+	std::cout << "-------------------------------------------------------" << std::endl;
+	for (int i = 0; i < MAX_VAL; i++)
+	{
+		std::cout << "mirror: " << mirror[i] << " numbers: " << numbers[i] << std::endl;
 		if (mirror[i] != numbers[i])
 		{
 			std::cerr << "didn't save the same value!!" << std::endl;
@@ -43,11 +51,14 @@ int main(int, char**)
 	{
 		std::cerr << e.what() << '\n';
 	}
-
+	std::cout << "-------------------------------------------------------" << std::endl;
 	for (int i = 0; i < MAX_VAL; i++)
 	{
+		std::cout << "numbers1: " << numbers[i] << std::endl;
 		numbers[i] = rand();
+		std::cout << "numbers2: " << numbers[i] << std::endl;
 	}
+
 	delete [] mirror;
 	return 0;
 }

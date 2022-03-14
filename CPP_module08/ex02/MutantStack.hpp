3f3d8c78b38534bf95ce97cddd2011f121465214
Seoyoung Lee <selee@student.42lyon.fr>
2022-03-14 18:37:43 +0100

    cpp8 ex02 fix

--- CPP_module08/ex02/MutantStack.hpp
+++ CPP_module08/ex02/MutantStack.hpp
@@ -7,26 +7,27 @@
 # include <vector>
 # include <stack>
 
-// template<class T, Class C = deque<T> >
-// class std::stack {
+/*
+template<class T, Class C = deque<T> >
+class std::stack {
 
-//     protected:
-//         C c; //container
+    protected:
+        C c; //container
 
-//     public:
-//         typedef typename C::value_type value_type;
-//         typedef typename C::size_type size_type;
-//         typedef C container_type;
-//         explicit stack(const C& a = C()) : c(a){} // Inherit the constructor
-//         bool empty() const { return c.empty(); }
-//         size_type size() const { return c.size(); }
-//         value_type& top() const { return c.back(); }
-//         const value_type& top() const { return c.back(); }
-//         void push(const value_type& n) { c.push_back(n); }
-//         void pop() { c.pop_back(); }
+    public:
+        typedef typename C::value_type value_type;
+        typedef typename C::size_type size_type;
+        typedef C container_type;
+        explicit stack(const C& a = C()) : c(a){} // Inherit the constructor
+        bool empty() const { return c.empty(); }
+        size_type size() const { return c.size(); }
+        value_type& top() const { return c.back(); }
+        const value_type& top() const { return c.back(); }
+        void push(const value_type& n) { c.push_back(n); }
+        void pop() { c.pop_back(); }
 
-// };
-
+};
+*/
 
 template < typename T >
 class MutantStack: public std::stack<T> 
--- CPP_module08/ex02/main.cpp
+++ CPP_module08/ex02/main.cpp
@@ -5,28 +5,36 @@ int main()
 	MutantStack<int>	mstack;
 
 	
-	mstack.push(5);
-	mstack.push(17);
+	mstack.push(1);
+	mstack.push(2);
 
 	std::cout << "top: " << mstack.top() << std::endl;
 
-	mstack.pop();
-	std::cout << "size: " << mstack.size() << std::endl;
+	// mstack.pop();
+	std::cout << "size: " << mstack.size() << std::endl << std::endl;
 
 	mstack.push(3);
+	mstack.push(4);
 	mstack.push(5);
-	mstack.push(737);
+	mstack.push(6);
 	std::cout << "top: " << mstack.top() << std::endl;
 
-	mstack.push(0);
 	std::cout << "size: " << mstack.size() << std::endl;
 
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
 
-	++it;
-	--it;
+	std::cout << "it: " << *it << std::endl;
+	std::cout << "ite: " << *ite << std::endl;
+	std::cout << "++it: " << *++it << std::endl;
 
+	mstack.push(7);
+	std::cout << "ite: " << *ite << std::endl;
+
+	// ++it;
+	// --it;
+
+	std::cout << "======== < stack > ========" << std::endl;
 	while (it != ite)
 	{
 		std::cout << *it << std::endl;
